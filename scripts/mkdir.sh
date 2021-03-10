22c05d6da1166ebaf4cb74d79cb208712d1d4785
Juwon44 <juwonobat@gmail.com>
2021-03-10 07:18:50 -0500

    fixed pip compatibility error again

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,5 +1,5 @@
 mkdir -p /web
-curl -O https://bootstrap.pypa.io/2.7/get-pip.py
+curl -O https://bootstrap.pypa.io/pip/2.7/get-pip.py
 python get-pip.py --user
 python -m pip install --upgrade "pip < 21.0"
 python -m pip install Flask
